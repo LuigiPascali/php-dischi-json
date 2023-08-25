e6813708cbbabaf9eb44735f5da98244874f38d2
luigimariapascali <luigimariapascali@gmail.com>
2023-08-25 09:20:21 +0200

    exercise complete

--- assets/app.js
+++ assets/app.js
@@ -1,37 +1,43 @@
 // Inizializzazione Vue Js:
 const app = Vue.createApp({
 
-    data() {
+  data() {
 
-      return {
-        discs: [],
-        selectedDisc: null
-      };
+    return {
+      discs: [],
+      selectedDisc: null
+    };
+
+  },
+
+  mounted() {
+    this.fetchDiscs();
+  },
+
+  methods: {
+
+    fetchDiscs() {
+      axios.get('api/get_discs.php')
+
+      .then(response => {
+        this.discs = response.data;
+      })
+
+      .catch(error => {
+        console.error('Error fetching discs: ', error);
+      });
 
     },
 
-    mounted() {
-      this.fetchDiscs();
-    },
-
-    methods: {
-
-      fetchDiscs() {
-        axios.get('api/get_discs.php')
-
-        .then(response => {
-          this.discs = response.data;
-        })
-
-        .catch(error => {
-          console.error('Error fetching discs: ', error);
-        });
-
-      },
-
+    showDiscDetails(disc) {
+      this.selectedDisc = disc.description;
     }
-    
-  });
 
-  // Mostra l'applicazione nella pagina HTML
-  app.mount('#app');
+  }
+  
+});
+
+// Mostra l'applicazione nella pagina HTML
+app.mount('#app');
+
+  
